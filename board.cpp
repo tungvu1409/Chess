c947959fcf805cd6700c24be05467255e0d45db0
tungvu1409 <vutungkm@gmail.com>
2025-03-31 20:03:48 +0700

    cap nhat board.cpp

--- board.cpp
+++ board.cpp
@@ -2,14 +2,14 @@
 
 
 void Board::draw(SDL_Renderer* renderer) {
-    int tileSize = 75; // Kích thước mỗi ô
+    int tileSize = 75;
 
     for (int row = 0; row < 8; row++) {
         for (int col = 0; col < 8; col++) {
             if ((row + col) % 2 == 0) {
-                SDL_SetRenderDrawColor(renderer, 107, 142, 35, 255); // Màu trắng
+                SDL_SetRenderDrawColor(renderer, 107, 142, 35, 255);
             } else {
-                SDL_SetRenderDrawColor(renderer, 227, 218, 201, 255); // Màu đen
+                SDL_SetRenderDrawColor(renderer, 227, 218, 201, 255);
             }
 
             SDL_Rect rect = { col * tileSize, row * tileSize, tileSize, tileSize };
